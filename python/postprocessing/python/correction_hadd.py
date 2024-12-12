26c5c13ba3e864245bb471d9c53255dbf87e895a
Seungjun <stormjun55@gmail.com>
2024-12-12 13:57:26 +0900

    before add some line

--- python/postprocessing/python/correction_hadd.py
+++ python/postprocessing/python/correction_hadd.py
@@ -42,50 +42,32 @@ os.makedirs(output_dir, exist_ok=True)
 # Correction 및 Target 설정
 corrections = ["muon_id", "muon_iso", "electron_id", "electron_reco", "jet_jer", "plieup"]
 targets = ["up", "down"]
+setting = 59.6 * 1000
+scale_factors = {
+    "DYJetsToLL_M-50": setting * 6077.22,
+    "DYJetsToLL_M-10": setting * 18610,
+    "ST_s-channel": setting * 3.36,
+    "ST_t-channel_antitop": setting * 80.95,
+    "ST_t-channel_top_MC": setting * 136.02,
+    "ST_tW_antitop_MC": setting * 34.91,
+    "ST_tW_top_MC": setting * 34.97,
+    "TTTo2L2Nu_MC_2018": setting * 87.4,
+    "TTToHadronic_MC_2018": setting * 380,
+    "TTToSemiLeptonic_MC": setting * 364,
+    "WJetsToLNu_MC": setting * 61526.7,
+    "WW_MC_2018": setting * 118.7,
+    "WZ_MC_2018": setting * 47.13,
+    "ZZ_MC_2018": setting * 16.523
+}
+
 
 # 스케일 팩터를 결정하는 함수 (파일 이름에 따라 스케일 결정)
 def get_scale_factor(filename):
-    # 예시 로직: 파일명에 따라 다른 스케일 적용
-    # WZJets 관련 파일이면 1.2를 곱한다.
-    # Diboson 관련 파일이면 0.8을 곱한다.
-    # 기타는 1.0
-    setting = 59.6*1000
-    if "DYJetsToLL_M-50" in filename:
-        return setting * 6077.22
-    elif "DYJetsToLL_M-10" in filename:
-        return 59.6* 1000 * 18610
-    elif "DYJetsToLL_M-10" in filename:
-    	return 59.6* 1000 * 18610
-    elif "ST_s-channel" in filename:
-    	return 59.6* 1000 * 3.36
-    elif "ST_t-channel_antitop" in filename:
-    	return 59.6* 1000 * 80.95 
-    elif "ST_t-channel_top_MC" in filename:
-    	return 59.6* 1000 * 136.02
-    elif "ST_tW_antitop_MC" in filename:
-    	return 59.6* 1000 *34.91 
-    elif "ST_tW_top_MC" in filename:
-    	return 59.6* 1000 *34.97 
-    elif "TTTo2L2Nu_MC_2018" in filename:
-    	return 59.6* 1000 * 87.4
-    elif "TTToHadronic_MC_2018" in filename:
-    	return 59.6* 1000 * 380
-    elif "TTToSemiLeptonic_MC" in filename:
-    	return 59.6* 1000 * 364
-    elif "WJetsToLNu_MC" in filename:
-    	return 59.6* 1000 *61526.7 
-    elif "WW_MC_2018" in filename:
-    	return 59.6* 1000 *118.7 
-    elif "WZ_MC_2018" in filename:
-    	return 59.6* 1000 * 47.13 
-    elif "ZZ_MC_2018" in filename:
-    	return 59.6* 1000 *16.523 
-    #elif "" in filename:
-    #	return 59.6* 1000 * 
-    #elif "" in filename:
-    #	return 59.6* 1000 * 
-    else:
-        return 1.0
+    for pattern, factor in scale_factors.items():
+        if pattern in filename:
+            return factor
+    # 매칭되는 패턴이 없으면 1.0 반환
+    return 1.0
 
 # 파일 합치기
 for correction in corrections:
